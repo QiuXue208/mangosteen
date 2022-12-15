de11d60b03076da6bac2fe610acee0ee7881bd05
qiuxue <1309571292@qq.com>
2022-12-15 20:31:49 +0800

    feat: 第一个页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Counter } from './components/Counter'
 </script>
 
 <template>
--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,13 +1,16 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
 import s from './index.module.scss'
+import logo from '../../assets/icons/mangosteen.svg'
 
 export const Welcome = defineComponent({
   setup() {
     return () => <div class={s.wrapper}>
-        <header>header</header>
-        <main><RouterView /></main>
-        <footer>footer</footer>
+        <header>
+          <img src={logo} />
+          <h1>山竹记账</h1>
+        </header>
+        <main class={s.main}><RouterView /></main>
       </div>
   }
 })
