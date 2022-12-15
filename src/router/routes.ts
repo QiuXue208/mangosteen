5ad6e6e35be5945cdca645efe588024e3f31be92
qiuxue <1309571292@qq.com>
2022-12-15 17:50:03 +0800

    feat: 创建欢迎界面路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,11 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { Home } from '../views/home/index'
 import { Login } from '../views/login/index'
+import { Welcome } from '../views/welcome'
+import { First } from '../views/welcome/components/First'
+import { Fourth } from '../views/welcome/components/Fourth'
+import { Second } from '../views/welcome/components/Second'
+import { Third } from '../views/welcome/components/Third'
 
 const appPage: RouteRecordRaw[] = [
   {
@@ -12,6 +17,28 @@ const appPage: RouteRecordRaw[] = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/welcome',
+    component: Welcome,
+    children: [
+      {
+        path: 'first',
+        component: First
+      },
+      {
+        path: 'second',
+        component: Second
+      },
+      {
+        path: 'third',
+        component: Third
+      },
+      {
+        path: 'fourth',
+        component: Fourth
+      }
+    ]
   }
 ]
 
