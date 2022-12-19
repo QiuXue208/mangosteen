ca075c6e23f9b08e4efa0fb28d876a9822fb273e
qiuxue <1309571292@qq.com>
2022-12-19 22:45:03 +0800

    feat：完善开始记账页面

--- src/views/start/index.tsx
+++ src/views/start/index.tsx
@@ -1,16 +1,21 @@
-import { defineComponent } from "vue"
+import { defineComponent, ref, watch } from "vue"
 import { Button } from "../../components/Button"
 import { Center } from "../../components/Center"
 import { FloatButton } from "../../components/FloatButton"
 import { Icon } from "../../components/Icon"
 import { NavBar } from "../../components/Navbar"
+import { Overlay } from "../../components/Overlay"
 import s from './index.module.scss'
 
 export const Start = defineComponent({
  setup(){
+   const visible = ref<boolean>(false)
    return () => (<div class={s.wrapper}>
     <NavBar v-slots={{
-      icon: () => <Icon name='menu' />,
+      icon: () => <Icon name='menu' onClick={() => {
+        console.log(1)
+        visible.value = true
+      }} />,
       default: () => 'Pocket Account'
     }} />
     <Center class={s.center}>
@@ -18,6 +23,7 @@ export const Start = defineComponent({
     </Center>
     <Button>开始记账</Button>
     <FloatButton />
+    {visible.value && <Overlay onClose={() => visible.value = false} />}
   </div>)
  }
 })
