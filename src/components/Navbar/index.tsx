1d5fec2536080bcd0fc1387ae81896fca6a2495b
qiuxue <1309571292@qq.com>
2022-12-19 21:31:37 +0800

    feat: NavBar组件

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from "vue"
+import s from './index.module.scss'
+export const NavBar = defineComponent({
+  setup(_, { slots }){
+    return () => (<div class={s.navbar}>
+      <span class={s.icon_wrapper}>{slots?.icon?.()}</span>
+      <span class={s.title_wrapper}>{slots?.default?.()}</span>
+    </div>)
+  }
+})
