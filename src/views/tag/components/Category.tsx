cea1e09eae5f075771489c2a40ce485b662c972a
qiuxue <1309571292@qq.com>
2022-12-24 22:30:35 +0800

    feat: 标签分类组件支持class prefix

--- src/views/tag/components/Category.tsx
+++ src/views/tag/components/Category.tsx
@@ -5,13 +5,16 @@ import { categoryMap } from "../constants"
 import s from './Category.module.scss'
 
 export const Category = defineComponent({
-  setup(){
+  props: {
+    tagPrefix: String
+  },
+  setup(props){
     const activeKey = ref('entertainment')
     
     return () => (<Tabs v-model:activeKey={activeKey.value} tabClass={s.tab} tabActiveCalss={s.tab_active}>
       {categoryMap.map(category => <Tab key={category.key} title={category.title}>
         <div class={s.tags}>
-        {category.tags?.map(tag => <div class={s.tag}>
+        {category.tags?.map(tag => <div class={[s.tag, props.tagPrefix]}>
             <Icon class={s.icon} name={tag} />
           </div>)}
         </div>
