a4e48521f3032d7f24d2701666c7988f0f0cf733
qiuxue <1309571292@qq.com>
2022-12-24 22:59:48 +0800

    feat: Button组件扩展按钮颜色prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,23 @@
-import { defineComponent } from "vue"
+import { defineComponent, PropType } from "vue"
 import s from './index.module.scss' 
 
 interface Props {
   onClick?: (e: MouseEvent) => void
+  color?: 'default' | 'primary' | 'success' | 'warning' | 'danger'
 }
-export const Button = defineComponent<Props>({
- setup(_, { slots }){
-   return () => (<div class={s.wrapper}>
-    <button class={s.button}>{slots?.default?.()}</button>
-   </div>)
- }
+export const Button = defineComponent({
+  props: {
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
+    },
+    color: {
+      type: String as PropType<'default' | 'primary' | 'success' | 'warning' | 'danger'>,
+      default: 'primary'
+    }
+  },
+  setup(props, { slots }){
+    return () => (<div class={s.wrapper}>
+      <button class={[s.button, s[props.color]]}>{slots?.default?.()}</button>
+    </div>)
+  }
 })
