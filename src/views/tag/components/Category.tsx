694ad6dfd0d4c34df34c192e78ccfbea4dfff07b
qiuxue <1309571292@qq.com>
2022-12-24 23:03:24 +0800

    feat: 标签分类组件

--- src/views/tag/components/Category.tsx
+++ src/views/tag/components/Category.tsx
@@ -8,17 +8,20 @@ export const Category = defineComponent({
   props: {
     tagPrefix: String
   },
+  
   setup(props){
     const activeKey = ref('entertainment')
     
-    return () => (<Tabs v-model:activeKey={activeKey.value} tabClass={s.tab} tabActiveCalss={s.tab_active}>
-      {categoryMap.map(category => <Tab key={category.key} title={category.title}>
-        <div class={s.tags}>
-        {category.tags?.map(tag => <div class={[s.tag, props.tagPrefix]}>
-            <Icon class={s.icon} name={tag} />
-          </div>)}
-        </div>
-      </Tab>)}
-    </Tabs>)
+    return () => (<div class={s.categroy}>
+      <Tabs v-model:activeKey={activeKey.value} tabClass={s.tab} tabActiveCalss={s.tab_active}>
+        {categoryMap.map(category => <Tab key={category.key} title={category.title}>
+          <div class={s.tags}>
+          {category.tags?.map(tag => <div class={[s.tag, props.tagPrefix]}>
+              <Icon class={s.icon} name={tag} />
+            </div>)}
+          </div>
+        </Tab>)}
+      </Tabs>
+    </div>)
   }
 })
